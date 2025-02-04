abab6a651bea97ce6e2063e7fd9573eabccfccc3
Leonardo Soares <leonardoafonso1048@gmail.com>
2025-02-04 15:43:21 -0300

    feat: ajuste de rotas

--- App.tsx
+++ App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import 'react-native-gesture-handler'
-import 'react-native-reanimated'
-import { colors } from './src/styles/colors'
-import Toast from 'react-native-toast-message'
-import Loading from './src/components/Loading'
-import { initialize } from 'react-native-clarity';
-import MainStack from './src/navigation/routes/MainStack'
+import React, { useEffect } from 'react';
+import 'react-native-gesture-handler';
+import 'react-native-reanimated'; // Deve ser importado no topo
+import { colors } from './src/styles/colors';
+import Toast from 'react-native-toast-message';
+import Loading from './src/components/Loading';
 import { OneSignal } from 'react-native-onesignal';
-import { PermissionsAndroid, Platform, SafeAreaView, StatusBar, View } from 'react-native';
+import { initialize } from 'react-native-clarity';
+import MainStack from './src/navigation/routes/MainStack';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { GlobalContextProvider } from './src/context/GlobalContextProvider';
-import { DMSans_400Regular, DMSans_500Medium, DMSans_700Bold } from '@expo-google-fonts/dm-sans'
-import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter'
-import { OpenSans_400Regular, OpenSans_500Medium, OpenSans_700Bold } from '@expo-google-fonts/open-sans'
-import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
-import { Roboto_500Medium } from '@expo-google-fonts/roboto'
+import {
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+} from '@expo-google-fonts/dm-sans';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import {
+  OpenSans_400Regular,
+  OpenSans_500Medium,
+  OpenSans_700Bold,
+} from '@expo-google-fonts/open-sans';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+import { Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,32 +47,46 @@ export default function App() {
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_600SemiBold,
-    Roboto_500Medium
+    Roboto_500Medium,
   });
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      initialize('k80ijj83lf'); // Inicialização do Clarity
+    }
+
+    // Inicialização do OneSignal
+    OneSignal.initialize('a903186d-f2c1-4e58-b11c-b15b8f1ec590');
+    OneSignal.Notifications.requestPermission(true);
+  }, []);
 
   if (!fontsLoaded) {
     return <Loading />;
   }
-  if (Platform.OS === 'android') {
-    initialize("k80ijj83lf")
-  }
-
-  // OneSignal Initialization
-  OneSignal.initialize("a903186d-f2c1-4e58-b11c-b15b8f1ec590");
-  OneSignal.Notifications.requestPermission(true);
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
-      <GlobalContextProvider>
-        <View className="relative flex-1">
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <GlobalContextProvider>
           <MainStack />
           <Toast />
-        </View>
-      </GlobalContextProvider>
-      <StatusBar
-        barStyle={'dark-content'}
-        backgroundColor="transparent"
-      />
-    </SafeAreaView>
+        </GlobalContextProvider>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+});
