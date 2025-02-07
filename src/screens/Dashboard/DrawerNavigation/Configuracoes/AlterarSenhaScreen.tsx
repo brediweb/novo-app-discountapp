d558d9abed815c42d14a7330ac005edc4589e0fe
Leonardo Soares <leonardoafonso1048@gmail.com>
2025-02-07 09:26:20 -0300

    bug(auth): alteração de senha

--- src/screens/Dashboard/DrawerNavigation/Configuracoes/AlterarSenhaScreen.tsx
+++ src/screens/Dashboard/DrawerNavigation/Configuracoes/AlterarSenhaScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { View } from 'react-native'
 import Toast from 'react-native-toast-message'
 import H5 from '../../../../components/typography/H5'
@@ -7,38 +7,107 @@ import Caption from '../../../../components/typography/Caption'
 import InputOutlined from '../../../../components/forms/InputOutlined'
 import FilledButton from '../../../../components/buttons/FilledButton'
 import MainLayoutAutenticado from '../../../../components/layout/MainLayoutAutenticado'
+import { api } from 'src/service/api'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function AlterarSenhaScreen() {
-  const { navigate } = useNavigate()
+  const { navigate, goBack } = useNavigate()
   const [senha, setSenha] = useState('')
+  const [loading, setLoading] = useState(false)
   const [senhaAtual, setSenhaAtual] = useState('')
   const [confirmarSenha, setConfirmarSenha] = useState('')
 
   async function onSubmit() {
-    Toast.show({
-      type: 'success',
-      text1: 'Dados alterados com sucesso!',
-    });
-    navigate('ConfiguracoesScreen')
+    if (senha.length < 8) {
+      Toast.show({
+        type: 'error',
+        text1: 'A senha deve ter no mínimo 8 caracteres',
+      });
+      return;
+    }
+    if (senha !== confirmarSenha) {
+      Toast.show({
+        type: 'error',
+        text1: 'As senhas não coincidem',
+      });
+      return;
+    }
+    setLoading(true)
+    const jsonUsuario = await AsyncStorage.getItem('infos-user') as any
+    if (jsonUsuario) {
+      const novo_json = JSON.parse(jsonUsuario)
+      try {
+        if (novo_json.tipo_usuario === 'Anunciante') {
+          const response = await api.post('/altera/anunciante', {
+            password: senha,
+            password_confirmation: confirmarSenha
+          }, {
+            headers: {
+              Authorization: `Bearer ${novo_json.token}`
+            }
+          })
+          Toast.show({
+            type: 'success',
+            text1: 'Senha alterada com sucesso!',
+          });
+          goBack()
+        } else if (novo_json.tipo_usuario === 'cliente') {
+          const response = await api.post('/altera/cliente', {
+            password: senha,
+            password_confirmation: confirmarSenha
+          }, {
+            headers: {
+              Authorization: `Bearer ${novo_json.token}`
+            }
+          })
+          Toast.show({
+            type: 'success',
+            text1: 'Senha alterada com sucesso!',
+          });
+          goBack()
+        } else {
+          Toast.show({
+            type: 'error',
+            text1: 'Verifique sua conexão com a internet',
+          });
+        }
+      } catch (error: any) {
+        Toast.show({
+          type: 'error',
+          text1: 'Erro ao alterar senha',
+          text2: error.message,
+        });
+      }
+      setLoading(false)
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: 'Verifique sua conexão com a internet !',
+      });
+      setLoading(false)
+    }
   }
-  
+
   return (
-    <MainLayoutAutenticado bottomDrawer >
+    <MainLayoutAutenticado bottomDrawer loading={loading}>
       <H5>Alterar Senha</H5>
       <Caption>Altera a sua senha</Caption>
       <View className='mt-6 mb-10'>
-        <InputOutlined
+        {/* <InputOutlined
           keyboardType={''}
+          secureTextEntry
           label='Senha atual'
           onChange={setSenhaAtual}
-        />
+        /> */}
         <InputOutlined
           keyboardType={''}
           label='Nova senha'
+          secureTextEntry
           onChange={setSenha}
         />
         <InputOutlined
           keyboardType={''}
+          secureTextEntry
           label='Confirmar nova senha'
           onChange={setConfirmarSenha}
         />
