fbf15c0f3ad598b6519764c8a66772cf869374be
Leonardo Soares <leonardoafonso1048@gmail.com>
2025-08-01 12:19:34 -0300

    feat(anunciante): consumo com extratos

--- src/screens/Dashboard/DrawerNavigation/Cliente/Checkout/ClienteConsumoScreen.tsx
+++ src/screens/Dashboard/DrawerNavigation/Cliente/Checkout/ClienteConsumoScreen.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import { useEffect, useState } from 'react'
 import { api } from '../../../../../service/api'
 import { colors } from '../../../../../styles/colors'
@@ -10,6 +10,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import HeaderPrimary from '../../../../../components/header/HeaderPrimary'
 import CardConsumo from '../../../../../components/cards/Cliente/CardConsumo'
 import MainLayoutAutenticado from '../../../../../components/layout/MainLayoutAutenticado'
+import Paragrafo from '@components/typography/Paragrafo'
+import { createNumberMask } from 'react-native-mask-input'
+import { format } from 'date-fns'
 
 interface PropsConsumo {
   total_gasto: string,
@@ -17,12 +20,28 @@ interface PropsConsumo {
   cupons_favoritos: number,
   cupons_consumidos: number,
   cupons_disponiveis: number,
+  extrato: [
+    {
+      codigo_pix: string
+      data: string
+      endereco_entrega: string
+      forma_pagamento: string
+      id: any
+      status: string
+      total: any
+      uuid: string
+    },
+  ]
 }
 
 export default function ClienteConsumoScreen() {
   const isFocused = useIsFocused()
   const [loading, setLoading] = useState(true)
   const [dadosConsumo, setDadosConsumo] = useState<PropsConsumo>()
+
+
+  console.log(typeof dadosConsumo?.extrato[0].total);
+
 
   async function getConsumo() {
     setLoading(true)
@@ -41,6 +60,43 @@ export default function ClienteConsumoScreen() {
     }
     setLoading(false)
   }
+
+  const formatarMoeda = (texto: string | number) => {
+    const textoStr = String(texto); // força ser string
+
+    const somenteNumeros = textoStr.replace(/\D/g, '');
+    const numero = parseFloat(somenteNumeros);
+
+    if (isNaN(numero)) return '';
+
+    return numero.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  };
+
+  function dataFormatada(dataPura: any) {
+    const dataOriginal = new Date(dataPura);
+    return format(dataOriginal, 'dd/MM/yyyy');
+  }
+
+  // Função para formatar nome do mês e ano
+  const obterMesAno = (data: string) => {
+    const date = new Date(data);
+    return date.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' });
+  };
+
+  // Agrupar por mês/ano
+  const agruparPorMes = (extratos: any[]) => {
+    return extratos.reduce((acc, extrato) => {
+      const mesAno = obterMesAno(extrato.data);
+      if (!acc[mesAno]) {
+        acc[mesAno] = [];
+      }
+      acc[mesAno].push(extrato);
+      return acc;
+    }, {} as Record<string, any[]>);
+  };
 
   useEffect(() => {
     getConsumo()
@@ -102,6 +158,39 @@ export default function ClienteConsumoScreen() {
           titulo='Cupons favoritos'
           quantidade={dadosConsumo?.cupons_favoritos ?? "0"}
         />
+
+        <View className='mt-8'>
+          <H3 color={colors.tertiary20} >Extrato Completo</H3>
+        </View>
+        <View className='mt-2'>
+          {dadosConsumo && (() => {
+            const agrupado = agruparPorMes(dadosConsumo.extrato);
+
+            return Object.entries(agrupado).map(([mes, extratos]) => (
+              <View key={mes} className="mb-3">
+                <View className='w-full bg-gray-400 h-[1px]' />
+                <Text className="text-lg text-center font-bold">{mes}</Text>
+                <View className='w-full bg-gray-400 h-[1px]' />
+
+                {extratos.map((extrato: any, index: any) => (
+                  <View key={index} className="mt-3">
+                    <Paragrafo title={`Data: ${dataFormatada(extrato.data)}`} />
+                    <View
+                      className="mt-2 border-solid border-2 px-2 py-4 rounded-xl"
+                      style={{ borderColor: colors.secondary70 }}
+                    >
+                      <H3 color={colors.secondary20}>{formatarMoeda(extrato.total)}</H3>
+                      <H1 fontWeight={'bold'} fontsize={16} title={`Status:`} />
+                      <Paragrafo title={`${extrato.status}`} />
+                      <H1 fontWeight={'bold'} fontsize={16} title={`Forma de pagamento:`} />
+                      <Paragrafo title={`${extrato.forma_pagamento}`} />
+                    </View>
+                  </View>
+                ))}
+              </View>
+            ));
+          })()}
+        </View>
 
       </View>
     </MainLayoutAutenticado>
